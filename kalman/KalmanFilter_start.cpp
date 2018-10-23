7275721493754db8c6ddd2dba3260e8336f679f8
Giovanni De Toni <giovanni.det@gmail.com>
2018-10-23 12:49:45 +0200

    Add implementation of kalman to predict mouse position (without acceleration).

--- kalman/KalmanFilter_start.cpp
+++ kalman/KalmanFilter_start.cpp
@@ -29,9 +29,119 @@ line( img, Point( center.x + d, center.y - d ),                \
 Point( center.x - d, center.y + d ), color, 2, CV_AA, 0 )
 
 int main () {
-    
 
+	// Matrix in which to plot the trajectories (coloured)
+	Mat img(1000, 1000, CV_8UC3);
 
-    return 0;
+	// Our Kalman filter
+	// M = [x_m, y_m]
+	// S = [x, y, v_x, v_y] (we record the velocity in both directions)
+	KalmanFilter KF(4,2,0);
+
+	// Matrices that will contain the state
+	// and the noise. Here we show two ways to
+	// create a matrix containing floats.
+	Mat_<float> state(4,1);
+	Mat processNoise(4,1, CV_32F);
+
+	// Contains the position of the mouse. Kalman does
+	// not require the initial position, but we need to set
+	// it anyway
+	Mat_<float> measurement(2,1);
+	measurement.setTo(Scalar(0));
+
+	// We will detect the mouse position inside this window.
+	// We set the function on_mouse on the newly created window.
+	namedWindow("Kalman");
+	setMouseCallback("Kalman", on_mouse, 0);
+
+	// Set up the initial state of the Kalman filter
+	// (we do not set the velocity).
+	KF.statePre.at<float>(0) = mouse_info.x;
+	KF.statePre.at<float>(1) = mouse_info.y;
+	KF.statePre.at<float>(2) = 0;
+	KF.statePre.at<float>(3) = 0;
+
+	// We need then to define the transition matrix.
+	// It has to be 4x4 in order to get the following equations:
+	// -> x = x_0 + t*v_x
+	// -> y = y_0 + t*v_y
+	// -> v_x = v_0x
+	// -> v_u = v_0y
+	// Therefore, T must be:
+	// 1 0 1 0
+	// 0 1 0 1
+	// 0 0 1 0
+	// 0 0 0 1
+	// such that T*S will give us those equations.
+	KF.transitionMatrix = *(Mat_<float>(4,4) <<
+	1,0,1,0,
+	0,1,0,1,
+	0,0,1,0,
+	0,0,0,1);
+
+	// Set the measurement function to the identity
+	// and to the process noise covariance. The covariance
+	// will have slightly smaller values on the diagonal.
+	setIdentity(KF.measurementMatrix);
+	setIdentity(KF.processNoiseCov, Scalar::all(1e-4));
+	setIdentity(KF.measurementNoiseCov, Scalar::all(0.1));
+	setIdentity(KF.errorCovPost, Scalar::all(0.1));
+
+	// Clear the vectors
+	mousev.clear();
+	kalmanv.clear();
+
+	// Update process
+	while(true) {
+
+		// Predict. Updates the statePre variable.
+		// We store also its value to plot it later
+		// (basically its x and y coordinates).
+		Mat prediction = KF.predict();
+		Point predictionPt(prediction.at<float>(0), prediction.at<float>(1));
+
+		// Measurement
+		measurement(0) = mouse_info.x;
+		measurement(1) = mouse_info.y;
+
+		// Store the mouse position to plot it.
+		Point measPt(measurement(0), measurement(1));
+		mousev.push_back(measPt);
+
+		// Update phase
+		Mat estimated = KF.correct(measurement);
+
+		// Store the kalman predicted point
+		Point statePt(estimated.at<float>(0), estimated.at<float>(1));
+		kalmanv.push_back(statePt);
+
+		img = Scalar::all(0);
+
+		// State point plotted in white
+		// Measured point will be plotted in red
+		// Predicted point will be plotted in green
+		drawCross(statePt, Scalar(255, 255, 255), 5);
+		drawCross(measPt, Scalar(0,0,255), 5);
+		drawCross(predictionPt, Scalar(0,255,0), 5);
+
+		// Print a line over all the points
+		for (int i = 0; i < mousev.size()-1; ++i) {
+			line(img, mousev[i], mousev[i+1], Scalar(255,0,0));
+		}
+
+		// Printe the same for the kalman points
+		// (prediction of the kalman filter)
+		for (int j = 0; j < kalmanv.size()-1; ++j) {
+			line(img, kalmanv[j], kalmanv[j+1], Scalar(0,255,0));
+		}
+
+		// Show the image
+		imshow("Kalman", img);
+		waitKey(100);
+
+	}
+
+	return 0;
 }
 
